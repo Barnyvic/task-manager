45f496654cb143d84681f0119f1deed03e4ab562
Barny Victor <victorbarny4@gmail.com>
2023-11-08 18:39:08 +0100

    finished setting up

--- src/__tests__/task-controller.test.ts
+++ src/__tests__/task-controller.test.ts
@@ -14,10 +14,20 @@ afterAll(async () => {
 });
 
 describe("Task Manager", () => {
-  it("should create a task", async () => {
+ it("should create a task", async () => {
+    const taskData = { title: "Test Task", description: "A test task" };
+
     const response = await api
       .post("/api/tasks")
-      .send({ title: "Test Task", description: "A test task" });
-    expect(response.status).toBe(200);
+      .send(taskData);
+
+console.log(response.body);
+
+
+    // expect(response.status).toBe(200);
+
+    expect(response.body.title).toBe(taskData.title);
+    expect(response.body.description).toBe(taskData.description);
+    expect(response.body.completed).toBe(false);
   });
 });
